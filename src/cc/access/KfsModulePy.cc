19d0bcc913ed2ffb310dbe10cc44977c41060299
sriramsrao <sriramsrao@8bf026d7-4dfc-6da9-1e29-2836fb35e766>
2007-09-14 23:35:58 +0000

    -- Moving the projec to Apache v. 2.0 license.

--- src/cc/access/KfsModulePy.cc
+++ src/cc/access/KfsModulePy.cc
@@ -4,22 +4,21 @@
 // Created 2006/08/01
 // Author: Blake Lewis (Kosmix Corp.) 
 //
-// Copyright (C) 2006 Kosmix Corp.
+// Copyright 2006 Kosmix Corp.
 //
 // This file is part of Kosmos File System (KFS).
 //
-// KFS is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation under version 3 of the License.
+// Licensed under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
 //
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see
-// <http://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
 // 
 // \brief Glue code for Python apps to access KFS.
 //
