12a3c8e3fe4821f4c4ba1cbcdec90e6f6d67d2cc
sriramsrao <sriramsrao@8bf026d7-4dfc-6da9-1e29-2836fb35e766>
2008-07-10 01:05:47 +0000

    - Bug fix: The readdir op maintains pointers to MetaDentry.  If the MetaDentry    is deleted before the readdir finishes, memory corruption ensues.

--- trunk/src/cc/meta/request.h
+++ trunk/src/cc/meta/request.h
@@ -262,7 +262,7 @@ struct MetaRmdir: public MetaRequest {
  */
 struct MetaReaddir: public MetaRequest {
 	fid_t dir;	//!< directory to read
-	vector <MetaDentry *> v; //!< vector of results
+	vector <MetaDentry> v; //!< vector of results
 	MetaReaddir(seq_t s, fid_t d):
 		MetaRequest(META_READDIR, s, false), dir(d) { }
 	int log(ofstream &file) const;
