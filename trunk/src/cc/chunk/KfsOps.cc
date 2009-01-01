e5ee1316457b2173b2ff1f320ef8acb1ee2d9651
lohitvijayarenu <lohitvijayarenu@8bf026d7-4dfc-6da9-1e29-2836fb35e766>
2009-01-01 20:40:09 +0000

    KFS Fsck Initial Version.

--- trunk/src/cc/chunk/KfsOps.cc
+++ trunk/src/cc/chunk/KfsOps.cc
@@ -2007,16 +2007,14 @@ PingOp::Response(ostringstream &os)
 void
 DumpChunkMapOp::Response(ostringstream &os)
 {
-    os << "OK\r\n";
-    os << "Cseq: " << seq << "\r\n";
-    os << "Status: " << status << "\r\n\r\n";
-	/*
     ostringstream v;
     gChunkManager.DumpChunkMap(v);
+    os << "OK\r\n";
+    os << "Cseq: " << seq << "\r\n";
+    os << "Status: " << status << "\r\n";
     os << "Content-length: " << v.str().length() << "\r\n\r\n";
     if (v.str().length() > 0)
        os << v.str();
-	*/
 }
 
 void
