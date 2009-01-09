128b13b014f68f641354a562789f8da044d36a0a
sriramsrao <sriramsrao@8bf026d7-4dfc-6da9-1e29-2836fb35e766>
2009-01-09 21:25:09 +0000

    -- Adding in the emulator code (the emulator is used for fsck).

---
+++ trunk/src/cc/emulator/LayoutEmulator.h
@@ -0,0 +1,83 @@
+//---------------------------------------------------------- -*- Mode: C++ -*-
+// $Id: LayoutEmulator.h $
+//
+// Created 2008/08/08
+//
+// Author: Sriram Rao
+//
+// Copyright 2008 Quantcast Corp.
+//
+// This file is part of Kosmos File System (KFS).
+//
+// Licensed under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// \brief Emulator for the layout manager: read in a chunk->location
+// map; we can then migrate blocks around to experiment with placement algorithms.
+//
+//----------------------------------------------------------------------------
+
+#ifndef EMULATOR_LAYOUTEMULATOR_H
+#define EMULATOR_LAYOUTEMULATOR_H
+
+#include <string>
+#include <map>
+#include <tr1/unordered_map>
+#include "meta/LayoutManager.h"
+
+namespace KFS
+{
+
+    class LayoutEmulator : public LayoutManager {
+    public:
+        LayoutEmulator() {
+            SetMinChunkserversToExitRecovery(0);
+            ToggleRebalancing(true);
+        };
+
+        // Given a chunk->location data in a file, rebuild the chunk->location map.
+        //
+        int LoadChunkmap(const std::string &chunkLocationFn, bool addChunksToReplicationChecker = false);
+
+        void AddServer(const ServerLocation &loc, int rack, float totalSpaceGB);
+
+        void SetupForRebalancePlanning() {
+            mDoingRebalancePlanning = true;
+        }
+        int SetRebalancePlanOutFile(const std::string &rebalancePlanFn);
+        int BuildRebalancePlan();
+
+        void ExecuteRebalancePlan();
+
+        void PrintChunkserverBlockCount();
+
+        void ReadNetworkDefn(const std::string &networkFn);
+
+        int VerifyRackAwareReplication(bool checkSize, bool verbose);
+
+        seq_t GetChunkversion(fid_t fid, chunkId_t cid);
+
+        size_t GetChunkSize(chunkId_t cid);
+
+        vector<size_t> GetChunkSizes(chunkId_t cid);
+
+        void MarkServerDown(const ServerLocation &loc);
+    private:
+        void Parse(const char *line, bool addChunksToReplicationChecker);
+        bool mDoingRebalancePlanning;
+        std::tr1::unordered_map<chunkId_t, std::vector<size_t> > mChunkSize;
+    };
+
+    extern LayoutEmulator gLayoutEmulator;
+}
+
+#endif // EMULATOR_LAYOUTEMULATOR_H
